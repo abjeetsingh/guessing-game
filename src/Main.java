098635db953c8b470fc791b319d4d248c2f2cc9f
Abjeet Singh <abjeets50@gmail.com>
2023-08-16 03:57:56 +0530

    feat: add  guess number

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        {
+            Scanner input = new Scanner(System.in);
+            Random rand = new Random();
+            int numberToGuess = rand.nextInt(100) + 1;
+            int guess = 0;
+            int numGuesses = 0;
+
+            System.out.println("Guess a number between 1 and 100!");
+
+            while (guess != numberToGuess) {
+                guess = input.nextInt();
+                numGuesses++;
+
+                if (guess < numberToGuess) {
+                    System.out.println("Too low! Guess again.");
+                } else if (guess > numberToGuess) {
+                    System.out.println("Too high! Guess again.");
+                }
+            }
+
+            System.out.println("Congratulations! You guessed the number in " + numGuesses + " guesses.");
+        }
     }
 }
